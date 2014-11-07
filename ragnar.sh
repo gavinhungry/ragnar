1d064a2513dd5035a77cd4833ffa3ce8c19b66e8
Gavin Lloyd <gavinhungry@gmail.com>
2014-11-06 16:54:45 -0800

    Add more SSH control

--- ragnar.sh
+++ ragnar.sh
@@ -9,17 +9,34 @@ source $(dirname "${BASH_SOURCE}")/abash/abash.sh
 
 SERVER=${RAGNAR_SERVER:-subterfuge}
 NBDEXPORT=${RAGNAR_NBDEXPORT:-ragnar}
-KEYFILE=${RAGNAR_KEYFILE}
+KEYFILE=${RAGNAR_KEYFILE:-/etc/luks/${NBDEXPORT}.key}
 
 TMPDIR=/tmp/.nbd-${SERVER}-${NBDEXPORT}
-mkdir -p $TMPDIR
-
-nbd_device() {
-  cat "${TMPDIR}/nbd"
-}
+mkdir -p ${TMPDIR}
 
 ssh_pid() {
   cat "${TMPDIR}/ssh"
+}
+
+ssh_is_open() {
+  [ -f "${TMPDIR}/ssh" ] && ps -p $(ssh_pid) &> /dev/null
+}
+
+open_ssh() {
+  ssh -NnL 10809:127.0.0.1:10809 $SERVER &> /dev/null &
+  SSH_PID=$!
+  disown $SSH_PID
+  echo $SSH_PID > ${TMPDIR}/ssh
+}
+
+close_ssh() {
+  if ssh_is_open; then
+    kill -9 $(ssh_pid) &> /dev/null && rm "${TMPDIR}/ssh"
+  fi
+}
+
+nbd_device() {
+  cat "${TMPDIR}/nbd"
 }
 
 nbd_is_open() {
@@ -36,27 +53,44 @@ nbd_next_open() {
   done
 }
 
-ssh_is_open() {
-  [ -f "${TMPDIR}/ssh" ] && ps -p $(ssh_pid) &> /dev/null
-}
-
-close_ssh() {
-  if ssh_is_open; then
-    kill -9 $(ssh_pid) &> /dev/null && rm "${TMPDIR}/ssh"
-  fi
-}
-
 export_is_open() {
   [ -f "${TMPDIR}/nbd" ] || return 1
   nbd_is_open $(nbd_device)
+}
+
+open_export() {
+  sudo -v
+  sudo modprobe nbd
+  NBD=$1
+
+  if sudo nbd-client localhost /dev/$NBD -name $NBDEXPORT &> /dev/null; then
+    echo $NBD > ${TMPDIR}/nbd
+    msg "time to luksOpen, then mount"
+  else
+    close_ssh
+    rm -fr ${TMPDIR}
+    return 1
+  fi
 }
 
 close_export() {
   if export_is_open; then
     sudo -v
     sudo modprobe nbd
-    sudo nbd-client -d /dev/$(nbd_device) &> /dev/null
+    sudo nbd-client -d /dev/$(nbd_device) &> /dev/null && rm -f "${TMPDIR}/nbd"
   fi
+}
+
+luks_is_open() {
+  echo LUKS_IS_OPEN
+}
+
+luks_open() {
+  echo cryptsetup luksOpen
+}
+
+luks_close() {
+  echo cryptsetup luksClose
 }
 
 open() {
@@ -64,26 +98,12 @@ open() {
 
   msg "Opening SSH connection to ${SERVER}"
   close_ssh || die "Could not close existing SSH connection to ${SERVER}"
-  ssh -NnL 10809:127.0.0.1:10809 $SERVER &> /dev/null &
-  SSH_PID=$!
-  disown $SSH_PID
-  echo $SSH_PID > $TMPDIR/ssh
+  open_ssh
   sleep 1
 
   NBD=$(nbd_next_open)
-
   msg "Opening network block device on ${NBD}"
-  sudo -v
-  sudo modprobe nbd
-
-  if sudo nbd-client localhost /dev/$NBD -name $NBDEXPORT &> /dev/null; then
-    echo $NBD > $TMPDIR/nbd
-    msg "time to luksOpen, then mount"
-  else
-    close_ssh
-    rm -fr $TMPDIR
-    err "Error opening network block device"
-  fi
+  open_export $NBD || dir "Could not open network block device on ${NBD}"
 }
 
 close() {
@@ -91,13 +111,15 @@ close() {
   # luksClose
 
   export_is_open && msg "Closing network block device on $(nbd_device)"
-  close_export
+  close_export || die "Could not close network block device on $(nbd_device)"
 
   ssh_is_open && msg "Closing SSH connection to ${SERVER}"
-  close_ssh
+  close_ssh || die "Could not close existing SSH connection to ${SERVER}"
+
+  rm -fr ${TMPDIR}
 }
 
-# [ -z "$KEYFILE" ] && die "RAGNAR_KEYFILE must be specified"
+sudo [ -f "${KEYFILE}" ] || die "Keyfile not found"
 
 case $1 in
   'open') open ;;
